ef7f31778c7c29f39bc59226c792f2dc62e0fe9c
MCYJ <june1012june@gmail.com>
2022-12-18 03:12:25 +0900

    feat: BMI calculator
    - intent

---
+++ BMI calculator/app/src/main/java/com/yj/bmicalculator/ResultActivity.kt
@@ -0,0 +1,33 @@
+package com.yj.bmicalculator
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        Log.d("ResultActivity", "h: $height w: $weight")
+
+        // double 형 숫자 안에 pow() 메소드 존재
+        val bmi = weight / (height / 100.0).pow(2.0)
+        val resultText = when {
+            bmi >= 35.0 -> "고도 비만"
+            bmi >= 30.0 -> "중정도 비만"
+            bmi >= 25.0 -> "경도 비만"
+            bmi >= 20.0 -> "과체중"
+            bmi >= 15.0 -> "정상체중"
+            else -> "저체중"
+        }
+
+        val bmiValue: TextView = findViewById(R.id.bmiValueTextView)
+        bmiValue.text = bmi.toString()
+    }
+}
