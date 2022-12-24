db34f21bf33db73c8319b2f5bd495abff21a651a
MCYJ <june1012june@gmail.com>
2022-12-25 03:34:29 +0900

    feat : decorate for numbers
    - add drawble
    - using different color when it selected

--- Lottery Machine/app/src/main/java/com/yj/lotterymachine/MainActivity.kt
+++ Lottery Machine/app/src/main/java/com/yj/lotterymachine/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.yj.lotterymachine
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.contains
-import androidx.core.view.size
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private val addButton: Button by lazy {
@@ -60,6 +59,10 @@ class MainActivity : AppCompatActivity() {
         addButton.setOnClickListener {
                 initAddButton()
         }
+
+        clearButton.setOnClickListener {
+            runClearButton()
+        }
     }
 
     private fun initAddButton(){
@@ -75,6 +78,13 @@ class MainActivity : AppCompatActivity() {
 
         numList.add(numberPicker.value)
 
+        when (numberPicker.value) {
+            in 1..10 -> textList[numList.size-1].background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textList[numList.size-1].background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textList[numList.size-1].background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textList[numList.size-1].background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            in 41..45 -> textList[numList.size-1].background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+        }
 
 
         var numberList = numList.toList()
@@ -82,5 +92,19 @@ class MainActivity : AppCompatActivity() {
         numberList.forEachIndexed { idx, it ->
             textList[idx].text = it.toString()
         }
+    }
+
+    private fun runClearButton(){
+
+        var numberList = numList.toList()
+
+        numberList.forEachIndexed { idx, it ->
+            textList[idx].apply{
+                text = ""
+                setBackgroundColor(Color.WHITE)
+            }
+        }
+
+        numList.clear()
     }
 }
