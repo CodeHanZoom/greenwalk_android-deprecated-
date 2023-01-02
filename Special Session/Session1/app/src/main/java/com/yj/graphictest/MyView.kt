643fbd34980394817eef74f88f308a316ecd53da
MCYJ <june1012june@gmail.com>
2023-01-02 15:50:18 +0900

    feat: testapp for graphic

---
+++ Special Session/Session1/app/src/main/java/com/yj/graphictest/MyView.kt
@@ -0,0 +1,11 @@
+package com.yj.graphictest
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+
+class MyView (context: Context) : View(context) {
+    init {
+        setBackgroundColor(Color.YELLOW)
+    }
+}
