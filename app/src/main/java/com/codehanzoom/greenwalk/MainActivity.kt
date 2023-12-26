1bc28edef84eaa976cc9457a4f42a444c3708657
MCYJ <june1012june@gmail.com>
2023-12-27 01:15:52 +0900

    feat: MainPageActivity.kt - page after login process(MainActivity) - some change in colors.xml

--- app/src/main/java/com/codehanzoom/greenwalk/MainActivity.kt
+++ app/src/main/java/com/codehanzoom/greenwalk/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.codehanzoom.greenwalk
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.loginBtn).setOnClickListener {
+            var subIntent = Intent(this@MainActivity, MainPageActivity::class.java)
+            startActivity(subIntent)
+        }
     }
 }
